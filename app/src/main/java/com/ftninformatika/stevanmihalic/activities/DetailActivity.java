f3a6b25e1facbb02371ee64b8571b615fac9c1ae
Stevan Mihalic <stevanmihalic@gmail.com>
2019-03-19 20:07:07 +0100

    DetailActivity Methods(3)

--- app/src/main/java/com/ftninformatika/stevanmihalic/activities/DetailActivity.java
+++ app/src/main/java/com/ftninformatika/stevanmihalic/activities/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.ftninformatika.stevanmihalic.activities;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
@@ -23,8 +25,11 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,8 +48,10 @@ import com.j256.ormlite.dao.ForeignCollection;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
@@ -67,6 +74,16 @@ public class DetailActivity extends AppCompatActivity {
     private ForeignCollection<Oznake> oznakeForeignCollection = null;
     private List<Oznake> oznakeList = null;
     private ArrayAdapter<Oznake> arrayAdapterOznake = null;
+
+    private String prioritet = null;
+    private Spinner spinner = null;
+    private SpinnerAdapter spinnerAdapter = null;
+    private String[] nizPrioritet = null;
+
+    private String status = null;
+    private Spinner spinnerStatus = null;
+    private SpinnerAdapter spinnerAdapterStatus = null;
+    private String[] nizStatus = null;
 
 
     private Spannable message1 = null;
@@ -129,7 +146,7 @@ public class DetailActivity extends AppCompatActivity {
             oznakeForeignCollection = getDatabaseHelper().getGrupa().queryForId(idPosition).getOznake();
             oznakeList = new ArrayList<>(oznakeForeignCollection);
             arrayAdapterOznake = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, oznakeList);
-            listView.setAdapter(taskAdapter);
+            listView.setAdapter(arrayAdapterOznake);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -146,6 +163,186 @@ public class DetailActivity extends AppCompatActivity {
         }
 
     }
+
+    private void addTask() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setContentView(R.layout.add_task);
+        dialog.show();
+
+        nizPrioritet = getResources().getStringArray(R.array.PRIORITET);
+
+        spinner = dialog.findViewById(R.id.add_task_spinner_prioritet);
+        spinnerAdapter = new ArrayAdapter<>(DetailActivity.this, android.R.layout.simple_spinner_item, nizPrioritet);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                prioritet = String.valueOf(spinner.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        nizStatus = getResources().getStringArray(R.array.STATUS);
+
+        spinnerStatus = dialog.findViewById(R.id.add_task_spinner_status);
+        spinnerAdapterStatus = new ArrayAdapter<>(DetailActivity.this, android.R.layout.simple_spinner_item, nizStatus);
+        spinnerStatus.setAdapter(spinnerAdapterStatus);
+        spinnerStatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                status = String.valueOf(spinner.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        final EditText editNaziv = dialog.findViewById(R.id.add_task_naziv);
+        final EditText editOpis = dialog.findViewById(R.id.add_task_opis);
+        final EditText editKreirano = dialog.findViewById(R.id.add_task_vreme_kreiranja);
+        final EditText editZavrseno = dialog.findViewById(R.id.add_task_vreme_zavrsetka);
+
+        confirm = dialog.findViewById(R.id.add_task_button_confirm);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editNaziv.getText().toString().isEmpty()) {
+                    editNaziv.setError("Polje Naziv ne sme biti prazno");
+                    return;
+                }
+                if (editOpis.getText().toString().isEmpty()) {
+                    editOpis.setError("Polje Opis ne sme biti prazno");
+                    return;
+                }
+
+                String naziv = editNaziv.getText().toString();
+                String opis = editOpis.getText().toString();
+                String kreirano = editKreirano.getText().toString();
+                String zavrseno = editZavrseno.getText().toString();
+
+
+                try {
+                    intentPosition = getIntent();
+                    idPosition = intentPosition.getExtras().getInt("id");
+                    grupa = getDatabaseHelper().getGrupa().queryForId(idPosition);
+
+
+                    task = new Task();
+                    task.setNaziv(naziv);
+                    task.setOpis(opis);
+                    task.setVremeKreiranja(kreirano);
+                    task.setVremeZavrsetka(zavrseno);
+                    task.setTipPrioriteta(prioritet);
+                    task.setStatus(status);
+
+                    getDatabaseHelper().getToDoZadatak().create(task);
+                    dialog.dismiss();
+
+                    message1 = new SpannableString("Uspesno kreiran Zadatak sa nazivom: ");
+                    message2 = new SpannableString(task.getNaziv());
+                    spannableStyle();
+
+                    if (showMessage) {
+                        toast = Toast.makeText(DetailActivity.this, "", Toast.LENGTH_LONG);
+                        toastView = toast.getView();
+
+                        textToast = toastView.findViewById(android.R.id.message);
+                        textToast.setText(message1);
+                        textToast.append(message2);
+                        toast.show();
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        cancel = dialog.findViewById(R.id.add_task_button_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void izbrisiGrupu() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.izbrisi_grupu);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
+        intentPosition = getIntent();
+        idPosition = intentPosition.getExtras().getInt("id");
+
+        try {
+            grupa = getDatabaseHelper().getGrupa().queryForId(idPosition);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        TextView text = dialog.findViewById(R.id.izbrisi_grupu_text);
+        message1 = new SpannableString("Da li ste sigurni da zelite da izbrisete Grupu pod nazivom: ");
+        message2 = new SpannableString(grupa.getNaziv());
+        spannableStyle();
+        text.setText(message1);
+        text.append(message2);
+
+        confirm = dialog.findViewById(R.id.izbrisi_grupu_button_confirm);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+
+                try {
+
+                    taskForeignCollection = getDatabaseHelper().getGrupa().queryForId(idPosition).getToDoZadaci();
+                    taskList = new ArrayList<>(taskForeignCollection);
+
+                    getDatabaseHelper().getToDoZadatak().delete(taskList);
+                    getDatabaseHelper().getGrupa().delete(grupa);
+                    onBackPressed();
+
+                    message1 = new SpannableString("Uspesno izbrisana Grupa sa nazivom:  ");
+                    message2 = new SpannableString(grupa.getNaziv());
+                    spannableStyle();
+
+                    if (showMessage) {
+                        toast = Toast.makeText(DetailActivity.this, "", Toast.LENGTH_LONG);
+                        toastView = toast.getView();
+
+                        textToast = toastView.findViewById(android.R.id.message);
+                        textToast.setText(message1);
+                        textToast.append(message2);
+                        toast.show();
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        cancel = dialog.findViewById(R.id.izbrisi_grupu_button_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
 
     private void spannableStyle() {
         message1.setSpan(new StyleSpan(Typeface.BOLD), 0, message1.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -244,10 +441,10 @@ public class DetailActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.menu_detail_delete:
-
+                izbrisiGrupu();
                 break;
             case R.id.menu_detail_add_task:
-
+                addTask();
                 break;
             case R.id.menu_detail_update:
 
